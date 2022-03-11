c5e4e0b5d843d78188934ceffddd231845d05f29
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-03-10 18:36:41 -0700

    added tests for gallery and photo-list

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import photo from "../../assets/small/commercial/0.jpg";
+import Photolist from "../Photolist";
 
 function Gallery(props) {
-  const currentCategory = {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commerical projects",
-  };
+  const { currentCategory } = props;
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
-      <div className="flex-row">
-        <img
-          src={photo}
-          alt="Commercial Example"
-          className="img-thumbnail mx-1"
-        />
-      </div>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <Photolist category={currentCategory.name} />
     </section>
   );
 }
